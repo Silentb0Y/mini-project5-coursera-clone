f2b6ef6be1a1df486711ecd08ef551c25c494dba
Yash dhadve <yashdhadwe@gmail.com>
2023-10-14 13:36:34 +0530

    logIn and signIn page added

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Login } from './pages/login';
+import { SignIn } from './pages/signin';
 
 function App() {
   return (
     <div className="App">
-      hehe
+      <Router>
+        <Routes>
+          {/* <RoutLogine path='/' element={<Login setUserId={setUserId}/>}/> */}
+          <Route path='/' element={<Login />}/>
+          <Route path='/signIn' element={<SignIn />}/>
+          {/* <Route path='/chat' element={<ChatPage UserId={UserId} UserName={UserName} />}/> */}
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/pages/login.tsx
@@ -0,0 +1,49 @@
+
+import "bootstrap/dist/css/bootstrap.min.css"
+import { FormEvent, useState } from "react"
+
+interface LoginPageProps {
+    // setUserId: React.Dispatch<React.SetStateAction<number | undefined>>
+    // setLogIn: React.Dispatch<React.SetStateAction<boolean>>
+    // setUserName: React.Dispatch<React.SetStateAction<string>>
+}
+
+export const Login: React.FC<LoginPageProps> = ({  }) => {
+    const [Id, setId] = useState<string>('')
+    const [password, setpassword] = useState<string>('')
+
+    const sendLoginRequest = (event: FormEvent) => {
+    }
+
+    return (
+        <div>
+            <div className="container d-flex justify-content-center min-vh-100 align-items-center">
+                <div className="p-4 bg-white shadow-lg rounded-3 bg-gradient ">
+
+                    <form>
+                        <h1 className=" text-center ">LOGIN</h1>
+                        <div className="form-group">
+                            <label >Id</label>
+                            <input type="id" className="form-control" placeholder="Id"
+                                value={Id} onChange={e => setId(e.target.value)} />
+
+                        </div>
+                        <div className="form-group">
+                            <label >Password</label>
+                            <input type="text" className="form-control" id="exampleInputPassword1" placeholder="Password"
+                                value={password} onChange={e => setpassword(e.target.value)} />
+                        </div>
+
+                        <div className=" d-flex justify-content-center">
+                            <button type="submit" className="btn btn-primary m-4 " onClick={sendLoginRequest}>Submit</button>
+                        </div>
+                        <div className=" d-flex justify-content-center">
+                            <a href="/signIn">New user?</a>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+
+    )
+}
