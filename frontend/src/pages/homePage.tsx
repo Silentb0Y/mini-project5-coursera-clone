82d091eaf37d32bb8d8487ee348f051c4c4b1692
Yash dhadve <yashdhadwe@gmail.com>
2023-10-14 17:59:05 +0530

    homePage added

--- frontend/src/pages/homePage.tsx
+++ frontend/src/pages/homePage.tsx
@@ -1,16 +1,27 @@
 import "bootstrap/dist/css/bootstrap.min.css"
 
 export function HomePage() {
+    const CourseData = [{id: 1, name:'machine Learining', desc:"this is description of ml", link:"AddLinkHere"},
+                        {id: 2, name:'dsa', desc:"this is description of dsa", link:"AddLinkHere"},
+                        {id: 3, name:'cns', desc:"this is description of cns", link:"AddLinkHere"},
+                        {id: 4, name:'system design', desc:"this is description of sys", link:"AddLinkHere"}]
+
+    function SelectThisCourse(Id :number){
+
+    }
+    const listCourses = CourseData.map(course =>
+        <div key={course.id} onClick={() => SelectThisCourse(course.id)} className="card" style={{ width: '18rem' }}>
+            <div className="card-body">
+                <h5 className="card-title">{course.name}</h5>
+                <p className="card-text">{course.desc}</p>
+                <a href={course.link} className="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>
+    )
 
     return(
-        <div className="container p-4">
-            <div className="card" style={{ width: '18rem' }}>
-                <div className="card-body">
-                    <h5 className="card-title">Special title treatment</h5>
-                    <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                    <a href="#" className="btn btn-primary">Go somewhere</a>
-                </div>
-            </div>
+        <div className="container p-4 ">
+            {listCourses}
         </div>
     )
 }
